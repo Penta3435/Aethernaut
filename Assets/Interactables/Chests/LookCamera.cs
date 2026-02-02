82fbc52f47ba6798f0683e1c3b0bd02a323afecc
Penta3435 <pentakill3435@gmail.com>
2026-02-02 21:01:01 +0100

    island1234andInteractables

--- Assets/Interactables/Chests/LookCamera.cs
+++ Assets/Interactables/Chests/LookCamera.cs
@@ -2,19 +2,8 @@ using UnityEngine;
 
 public class LookCamera : MonoBehaviour
 {
-    [SerializeField] bool fixZ;
-
-    Quaternion originalRotation;
-    private void Awake()
-    {
-        originalRotation = transform.rotation;
-    }
     void Update()
     {
-        transform.LookAt(Camera.main.transform.position);
-        if(fixZ)
-        {
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
-        }
+        transform.rotation = Camera.main.transform.rotation;
     }
 }
--- Assets/Interactables/Interact.cs
+++ Assets/Interactables/Interact.cs
@@ -2,41 +2,39 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Chest : MonoBehaviour, IInteractables
+public class Interact : MonoBehaviour, IInteractables
 {
-    public UnityEvent OnOpen;
-    [SerializeField] Animator animator;
+    public UnityEvent OnInteract;
+    public bool infiniteInteracts = false;
     [SerializeField] GameObject FocusedIcon;
+    [SerializeField] private Animator animator;
+    [SerializeField] string animationStateName;
+
 
     public bool Focused { get; set; }
-    bool interactable = true;
 
-    int openId;
     TagHandle playerTag;
     private void Awake()
     {
-        openId = Animator.StringToHash("Open");
+        gameObject.layer = LayerMask.NameToLayer("Interactables");
         Focused = false;
         playerTag = TagHandle.GetExistingTag("Player");
     }
     void IInteractables.Interact()
     {
-        if (interactable)
+        OnInteract?.Invoke();
+        if(animator != null) animator.Play(animationStateName);
+        if (!infiniteInteracts)
         {
-            OnOpen.Invoke();
-            interactable = false;
-            animator.SetTrigger(openId);
+            FocusedIcon.SetActive(false);
+            Destroy(gameObject);
         }
-    }
-    public void Destroy()
-    {
-        Destroy(gameObject);
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag) && Focused == true)
         {
-            FocusedIcon.SetActive(true); 
+            FocusedIcon.SetActive(true);
             Focused = false;
         }
     }
